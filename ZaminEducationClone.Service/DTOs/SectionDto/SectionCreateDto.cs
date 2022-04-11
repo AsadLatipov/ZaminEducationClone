c1339429f298e9d513bab6d8c2ecfbd1e5200ad2
asad.latipov.dev@gmail.com <asad.latipov.dev@gmail.com>
2022-04-11 05:59:27 -0700

    fixed second bug

--- ZaminEducationClone.Service/DTOs/SectionDto/SectionCreateDto.cs
+++ ZaminEducationClone.Service/DTOs/SectionDto/SectionCreateDto.cs
@@ -5,13 +5,10 @@ namespace ZaminEducationClone.Service.DTOs.SectionDto
 {
     public class SectionCreateDto
     {
-        [JsonIgnore]
         public string NameUz { get; set; }
 
-        [JsonIgnore]
         public string NameRu { get; set; }
 
-        [JsonIgnore]
         public string NameEng { get; set; }
         public string Description { get; set; }
     }
